bb9acc9e59bf7f1661d8d8e20a929854903d0883
Łukasz Radecki <69908232+Lukas249@users.noreply.github.com>
2025-09-24 17:39:55 +0200

    refactor: add type definitions SubmissionStatus and SubmissionResult for getSubmissionResult function.

--- app/solve/submitCode.tsx
+++ app/solve/submitCode.tsx
@@ -1,7 +1,28 @@
 import { stdout } from "process";
 import { decodeUtf8Base64 } from "../lib/decodeBase64";
 
-export default async function getSubmissionResult (sourceCode: string, languageId: number, testcases: string) {
+type SubmissionStatus = {
+    id: number
+    description: string
+}
+
+export type SubmissionResult = {
+  stdout: string | null
+  stderr: string | null
+  compile_output: string | null
+  message: string | null
+  exit_code?: number | null
+  exit_signal?: number | null
+  status: SubmissionStatus
+  created_at?: Date | null
+  finished_at?: Date | null
+  token: string
+  time: number
+  wall_time?: number
+  memory: number
+}
+
+export default async function getSubmissionResult (sourceCode: string, languageId: number, testcases: string): Promise<SubmissionResult> {
     let intervalId: NodeJS.Timeout, timeoutId: NodeJS.Timeout;
 
     const cleanup = () => {
