ea866176eda096df8ca9915fb222cb3a43f2baca
Łukasz Radecki <69908232+Lukas249@users.noreply.github.com>
2025-10-04 19:32:00 +0200

    feat: enable askAI to process full conversation history with context

--- app/api/gemini-ai/route.tsx
+++ app/api/gemini-ai/route.tsx
@@ -1,22 +1,45 @@
+"server-only"
+
+import { MessageDetails } from "@/app/components/chat/types";
 import { GoogleGenAI } from "@google/genai";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  const { question } = await req.json();
+const GOOGLE_AI_STUDIO_API_KEY = process.env.GOOGLE_AI_STUDIO_API_KEY;
+const ai = new GoogleGenAI({ apiKey: GOOGLE_AI_STUDIO_API_KEY });
 
-  const GOOGLE_AI_STUDIO_API_KEY = process.env.GOOGLE_AI_STUDIO_API_KEY;
-  const ai = new GoogleGenAI({ apiKey: GOOGLE_AI_STUDIO_API_KEY });
-
-  const askAI = async (question: string) => {
-    const response = await ai.models.generateContent({
+const askAI = (messages: MessageDetails[], contexts?: Record<string, string>) => {
+  
+  return new Promise(async (resolve, reject) => {
+    ai.models.generateContent({
       model: "gemini-2.0-flash-001",
-      contents: question,
+      contents: messages.map((message) => ({ role: message.type === "question" ? "user" : "model", text: message.msg })),
+      config: {
+        systemInstruction:  `
+          ${
+            (contexts && Object.keys(contexts).length > 0) ?
+              `User provided some contexts: ${Object.entries(contexts).map(([type, context], i) => `${i + 1}. Type of context: ${type}, context: ${context}\n`)}` 
+              : 
+              ""
+          }
+        `
+      }
+    })
+    .then((res) => {
+      resolve(res.text)
+    })
+    .catch((e) => {
+      console.error('error name: ', e.name);
+      console.error('error message: ', e.message);
+      console.error('error status: ', e.status);
+      reject(e)
     });
+  }) 
+};
 
-    return response.text;
-  };
+export async function POST(req: NextRequest) {
+  const { messages, contexts } = await req.json();
 
-  const answer = await askAI(question);
+  const answer = await askAI(messages, contexts);
 
   return NextResponse.json(answer);
 }
