98280a21293ef3f98de831e1632aea06bec5a285
Łukasz Radecki <69908232+Lukas249@users.noreply.github.com>
2025-10-04 19:48:04 +0200

    feat: add context support to AI chat, update default text, and auto-scroll to last user message

--- app/components/chat/chat.tsx
+++ app/components/chat/chat.tsx
@@ -10,27 +10,41 @@ import React, {
 import Message from "./message";
 import { MessageDetails } from "./types";
 import Response from "./response";
+import ChipCloseable from "../chip-closeable";
+import Chip from "../chip";
+import _ from "lodash";
 import { ArrowUpIcon } from "@heroicons/react/24/outline";
 
 export type ChatRef = {
   addMessage: (message: MessageDetails) => void;
+  addContext: (type: string, context: Context) => void
+  getContexts: () => Record<string, Context>
 };
+
+export type Context = {
+  icon?: React.ReactNode;
+  text: string,
+  closeable: boolean
+}
 
 type Props = {
   ref: RefObject<ChatRef | null>;
-  placeholder?: string;
-  onSend?: (newMessage: MessageDetails) => void;
+  onSend?: (messages: MessageDetails[]) => void;
+  defaultMessages?: MessageDetails[]
+  defaultContexts?: Record<string, Context>
 };
 
-export default function Chat({ ref, placeholder, onSend }: Props) {
-  const [messages, setMessages] = useState<MessageDetails[]>([]);
+export default function Chat({ ref, onSend, defaultMessages = [], defaultContexts = {} }: Props) {
+  const [messages, setMessages] = useState<MessageDetails[]>(defaultMessages);
+  const [contexts, setContexts] = useState(defaultContexts)
   const [input, setInput] = useState("");
 
-  const bottomRef = useRef<HTMLDivElement>(null);
+  const chatMessagesRef = useRef<HTMLDivElement>(null)
+  const sendButtonRef = useRef<HTMLButtonElement>(null)
 
   const handleSend = (messageDetails: MessageDetails) => {
+    if (onSend) onSend([...messages, messageDetails]);
     addMessage(messageDetails);
-    if (onSend) onSend(messageDetails);
     setInput("");
   };
 
@@ -39,57 +53,123 @@ export default function Chat({ ref, placeholder, onSend }: Props) {
     setMessages((prev) => [...prev, messageDetails]);
   };
 
+  const addContext = (type: string, context: Context) => {
+    if(!context.text.trim()) return
+    setContexts((contexts) => {
+      const clone = {...contexts}
+      clone[type] = context
+      return clone
+    })
+  }
+
+  const getContexts = () => contexts;
+
+  const getMessagesHistory = () => messages
+
   useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    let i = messages.length - 1;
+
+    while(i >= 0 && messages[i].type !== "question") {
+      i--
+    }
+
+    if (i >= 0) {
+      const messageElement: HTMLSpanElement | null = document.querySelector(`#message${i}`)
+      messageElement?.scrollIntoView({ behavior: "smooth", block: "start"  });
+      chatMessagesRef.current?.scrollTo({
+        top: messageElement!.offsetTop - chatMessagesRef.current.offsetTop - 10,
+        behavior: "smooth"
+      });
+    }
   }, [messages]);
 
   useImperativeHandle(ref, () => {
     return {
       addMessage,
+      addContext,
+      getContexts
     };
   });
 
   return (
     <div className="bg-gray-dark flex h-full w-full flex-col p-1">
       <div className="relative flex max-h-full flex-1 flex-col justify-end overflow-y-auto">
-        <div className="flex flex-col items-end gap-2 overflow-auto p-3 py-4">
+        <div ref={chatMessagesRef} className="flex flex-col items-end gap-2 overflow-auto p-3 py-4 h-full relative">
           {messages.map((msgDetails, i) =>
             msgDetails.type == "question" ? (
-              <Message key={i}>{msgDetails.msg}</Message>
+              <Message id={`message${i}`} key={i}>{msgDetails.msg}</Message>
             ) : (
-              <Response key={i}>{msgDetails.msg}</Response>
+              <Response id={`message${i}`} key={i}>{msgDetails.msg}</Response>
             ),
           )}
-          <div ref={bottomRef}></div>
+          {!messages.length && (
+            <p className="text-gray absolute top-1/2 left-1/2 -translate-1/2 text-center text-xl">
+                <span>
+                  It's quiet here… ask me anything
+                </span>
+            </p>
+          )}
         </div>
-        {!messages.length ? (
-          <p className="text-gray absolute top-1/2 left-1/2 -translate-1/2 text-center text-xl">
-            {
-              "ASK AI ABOUT YOUR CODE, PROBLEM OR TESTCASES. YOU DON'T NEED TO PASTE ANYTHING."
-            }
-          </p>
-        ) : (
-          ""
-        )}
-        <div className="relative">
-          <input
-            className="border-gray w-full resize-none rounded-xl border-2 px-8 py-4 outline-none"
-            placeholder={placeholder}
+        <div className="border-gray border-2 my-2 px-3 py-2 rounded-xl">
+          {Object.keys(contexts).length > 0 && 
+            <div className="flex gap-1 flex-wrap">
+              {
+                Object.entries(contexts).map(([type, context], index) => {
+                  return (
+                    context.closeable ? 
+                      <ChipCloseable
+                        key={crypto.randomUUID()}
+                        icon={context.icon} 
+                        text={context.text} 
+                        className="border-gray text-white" 
+                        textClassName="text-xs"
+                        onClose={() => setContexts((contexts) => {
+                          const clone = _.cloneDeep(contexts)
+                          delete clone[type]
+                          return clone
+                        })}
+                      /> :
+                      <Chip
+                        key={crypto.randomUUID()}
+                        icon={context.icon} 
+                        text={context.text} 
+                        className="border-gray text-white" 
+                        textClassName="text-xs"
+                      />
+                  )
+                })
+              }
+            </div>
+          }
+          
+          <textarea
+            className="w-full resize-none my-2 outline-none py-3"
+            placeholder={"Shift+Enter for new line"}
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            onKeyUp={(e) => {
-              if (e.key === "Enter") {
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
                 e.preventDefault();
-                handleSend({ type: "question", msg: input });
-              }
+                sendButtonRef.current?.click()
+                return
+              } 
             }}
           />
-          <button
-            className="absolute top-1/2 right-2 -translate-y-1/2"
-            onClick={() => handleSend({ type: "question", msg: input })}
-          >
+          <div className="flex justify-end">
+            <button
+              ref={sendButtonRef}
+              className="cursor-pointer bg-white rounded-full p-2"
+              onClick={() => {
+                if(!input.trim()) {
+                  return
+                }
+
+                handleSend({ type: "question", msg: input })
+              }}
+            >
               <ArrowUpIcon className="font-bold text-gray-dark size-5 stroke-2" />
-          </button>
+            </button>
+          </div>
         </div>
       </div>
     </div>
