9b1b900de9a0bba1d8632646a3b813b5d2142ed2
Łukasz Radecki <69908232+Lukas249@users.noreply.github.com>
2025-10-04 22:19:40 +0200

    feat: support adding selected text as chat context and include feedbackAI in submission results

--- app/solve/page.tsx
+++ app/solve/page.tsx
@@ -16,6 +16,15 @@ import { askAI } from "../lib/ai";
 import Result, { resultType } from "./result";
 
 import { ToastContainer, toast } from "react-toastify";
+import AISelectionProvider from "../lib/AISelectionProvider";
+import { BeakerIcon, CodeBracketIcon, DocumentTextIcon } from "@heroicons/react/24/outline";
+
+type TabSection = "main" | "code" | "testcases";
+
+interface TabSetters {
+  setTabs:  React.Dispatch<React.SetStateAction<Tab[]>>;     
+  setCurrentTab: React.Dispatch<React.SetStateAction<number>>;
+}
 
 export default function Solve() {
   const [mainTabsCurrentTab, setMainTabsCurrentTab] = useState(0);
@@ -29,12 +38,22 @@ export default function Solve() {
   const [codeJudging, setCodeJudging] = useState(false);
 
   const chatRef = useRef<ChatRef>(null);
-  const chat = (
+
+  const [chat] = useState(
     <Chat
       ref={chatRef}
-      placeholder="ASK AI"
-      onSend={async (messageDetails: MessageDetails) => {
-        const answer = await askAI(messageDetails.msg);
+      onSend={async (messages: MessageDetails[]) => {
+        const chatContexts = chatRef.current?.getContexts()
+
+        const contexts: Record<string, string> = {}
+
+        if(chatContexts) {
+          for(const [key, value] of Object.entries(chatContexts)) {
+            contexts[key] = value.text
+          } 
+        } 
+
+        const answer = await askAI(messages, contexts);
         chatRef.current?.addMessage({ type: "response", msg: answer });
       }}
     />
@@ -45,17 +64,23 @@ export default function Solve() {
       id: crypto.randomUUID(),
       title: "Description",
       content: (
-        <ProblemDescription
-          id={problem.id}
-          title={problem.title}
-          description={problem.description}
-        />
+        <AISelectionProvider
+          buttonClickHandler={(__, selectedText) => {
+            chatRef.current?.addContext("description", {icon: <DocumentTextIcon className="size-3.5 fill-transparent stroke-primary" />, text: selectedText, closeable: true})
+          }}
+        >
+          <ProblemDescription
+            id={problem.id}
+            title={problem.title}
+            description={problem.description}
+          />
+        </AISelectionProvider>
       ),
       closeable: false,
     },
     {
       id: crypto.randomUUID(),
-      title: "AI Assistant",
+      title: "GraphlyAI",
       content: chat,
       closeable: false,
     },
@@ -66,6 +91,11 @@ export default function Solve() {
       id: crypto.randomUUID(),
       title: "Testcases",
       content: (
+        <AISelectionProvider
+          buttonClickHandler={(__, selectedText) => {
+            chatRef.current?.addContext("testcases", {icon: <BeakerIcon className="size-3.5 stroke-primary" />, text: selectedText, closeable: true})
+          }}
+        >
         <Editor
           height="100%"
           defaultLanguage="plaintext"
@@ -78,6 +108,7 @@ export default function Solve() {
             },
           }}
         />
+        </AISelectionProvider>
       ),
       closeable: false,
     },
@@ -88,55 +119,122 @@ export default function Solve() {
       id: crypto.randomUUID(),
       title: "Code",
       content: (
-        <Editor
-          height="100%"
-          defaultLanguage="c"
-          defaultValue={sourceCode}
-          theme="vs-dark"
-          onChange={(val) => val && setSourceCode(val)}
-          options={{
-            minimap: {
-              enabled: false,
-            },
+         <AISelectionProvider
+          buttonClickHandler={(__, selectedText) => {
+            chatRef.current?.addContext("code", {icon: <CodeBracketIcon className="size-3.5 stroke-primary" />,text: selectedText, closeable: true})
           }}
-          className="mb-2"
-        />
+        >
+          <Editor
+            height="100%"
+            defaultLanguage="c"
+            defaultValue={sourceCode}
+            theme="vs-dark"
+            onChange={(val) =>  val && setSourceCode(val)}
+            options={{
+              minimap: {
+                enabled: false,
+              },
+            }}
+            className="mb-2"
+          />
+        </AISelectionProvider>
       ),
       closeable: false,
     },
   ]);
 
-  const addResultTab = (result: SubmissionResult) => {
-    setMainTabs((tabs) => {
-      const deepClone = lodash.cloneDeep(tabs);
-      deepClone.push({
-        id: crypto.randomUUID(),
-        title: resultType(result),
-        content: <Result result={result} sourceCode={sourceCode} />,
-        closeable: true,
-      });
+  const getTabSetters = (section: TabSection): TabSetters => {
+    switch (section) {
+      case "main":
+        return { setTabs: setMainTabs, setCurrentTab: setMainTabsCurrentTab };
+      case "code":
+        return { setTabs: setCodeTabs, setCurrentTab: setCodeTabsCurrentTab };
+      case "testcases":
+        return { setTabs: setTestcasesTabs, setCurrentTab: setTestcasesTabsCurrentTab };
+    }
+  };
 
-      setMainTabsCurrentTab(() => deepClone.length - 1);
+  const addTab = (tab: Tab, tabSection: TabSection) => {
+    const { setTabs, setCurrentTab } = getTabSetters(tabSection)
+
+    setTabs((tabs) => {
+      const deepClone = lodash.cloneDeep(tabs);
+      deepClone.push(tab);
+      setCurrentTab(() => deepClone.length - 1);
       return deepClone;
     });
   };
 
   const handleRun = async () => {
-    await handleCodeRun(
+    const result = await handleCodeRun(
       problem.header + "\n" + sourceCode + "\n" + problem.driver,
       54,
       testcases.length,
       testcases,
     );
+
+    if(result) {
+      addTab(
+        {
+          id: crypto.randomUUID(),
+          title: resultType(result),
+          content: 
+              <Result result={result} sourceCode={sourceCode} />,
+          closeable: true,
+        },
+        "testcases"
+      )
+    }
   };
 
   const handleSubmit = async () => {
-    await handleCodeRun(
+    const result = handleCodeRun(
       problem.header + "\n" + sourceCode + "\n" + problem.driver,
       54,
       problem.testcases.length,
       problem.testcases.join("\n"),
     );
+
+    const feedbackAI = askAI(
+      [
+        {
+          type: "question",
+          msg: `You are given the result of a user code submission. Write short feedback that will be displayed directly in a feedback field. Do not include introductions, titles, or bullet points. Write in plain markdown as smooth flowing text. Always state the time complexity and space complexity in a natural sentence. Only add a brief improvement suggestion if there is something meaningful to improve. If nothing needs improvement, stop after giving complexities without adding filler.`
+        }
+      ],
+      {
+        "submission": `
+          Code: ${sourceCode}
+        `
+      }
+    );
+
+    Promise.all([
+      result, 
+      feedbackAI.then((answer) => answer).catch(() => "")
+    ])
+      .then(([result, feedbackAI]) => {
+        if(!result) {
+          throw new Error("Failed to submit code");
+        }
+
+        const content = feedbackAI ?
+          <Result result={result} sourceCode={sourceCode} feedbackAI={feedbackAI} /> :
+          <Result result={result} sourceCode={sourceCode} />
+
+        addTab(
+          {
+            id: crypto.randomUUID(),
+            title: resultType(result),
+            content,
+            closeable: true,
+          },
+          "main"
+        )
+      })
+      .catch(() => {
+        toast.error("Failed to submit code");
+      })
   };
 
   const handleCodeRun = async (
@@ -146,10 +244,8 @@ export default function Solve() {
     testcases: string,
   ) => {
     setCodeJudging(true);
-    getSubmissionResult(sourceCode, langId, testcasesNumber + "\n" + testcases)
-      .then((result) => {
-        addResultTab(result);
-      })
+
+    return getSubmissionResult(sourceCode, langId, testcasesNumber + "\n" + testcases)
       .catch(() => {
         toast.error("Failed to submit code");
       })
