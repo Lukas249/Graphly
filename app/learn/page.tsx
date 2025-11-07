f900437b8216ce8d7f5caee4c89f5880fcdda7b3
Łukasz Radecki <69908232+Lukas249@users.noreply.github.com>
2025-11-07 22:56:26 +0100

    Refactor /learn page to align with /visualize and /solve

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,5 +1,44 @@
-import { redirect, RedirectType } from "next/navigation";
+"use server";
 
-export default function Learn() {
-  redirect("/learn/dfs", RedirectType.replace);
+import Link from "next/link";
+import Menu from "../menu";
+
+const articles = [
+  { title: "DFS", path: "/learn/dfs" },
+  { title: "BFS", path: "/learn/bfs" },
+  { title: "Dijkstra Algorithm - Shortest Path", path: "/learn/dijkstra" },
+  {
+    title: "Eulerian Path/Cycle",
+    path: "/learn/eulerian-path",
+  },
+  {
+    title: "Kosaraju Algorithm - Strongly Connected Components",
+    path: "/learn/kosaraju",
+  },
+];
+
+export default async function LearnPage() {
+  return (
+    <div className="min-h-screen">
+      <Menu />
+      <div className="mx-auto my-10 max-w-2xl">
+        <h2 className="text-xl">Articles</h2>
+        {articles.map((vis) => {
+          return (
+            <Link
+              key={vis.title}
+              href={vis.path}
+              prefetch={false}
+              className="cursor-pointer"
+            >
+              <div className="bg-gray-dark hover:border-primary my-2 flex h-10 items-center justify-between rounded-lg px-4 hover:border-2">
+                <span>{vis.title}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
