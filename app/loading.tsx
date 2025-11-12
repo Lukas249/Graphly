af2101fdbfe853da9d42bc0590314253288925bd
Łukasz Radecki <69908232+Lukas249@users.noreply.github.com>
2025-11-12 19:50:09 +0100

    feat: use single loading.tsx for all pages

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center gap-5">
+      <span className="loader">L &nbsp; ading</span>
+    </div>
+  );
+}
