9ed8732b8c8fb9b2848435b4e2525f117f4a2089
Łukasz Radecki <69908232+Lukas249@users.noreply.github.com>
2026-04-09 16:11:21 +0200

    feat(linkList): refactor LinkList component to improve type safety and modularity. Add linkTemplate function for rendering links.

--- app/components/linkList/linkList.tsx
+++ app/components/linkList/linkList.tsx
@@ -1,30 +1,60 @@
 import Link from "next/link";
 import { linkTemplates } from "./linkTemplates";
-import { LinkType, NavItem } from "./types";
+import { ArticleNavItem } from "@/app/lib/articles/types";
+import { ProblemNavItem } from "@/app/lib/problems/types";
+import { VisualizationNavItem } from "@/app/lib/visualizations/types";
 
-export function LinkList({
-  links,
-  prefetch = false,
-  slugPrefix,
-  linkType,
-}: {
-  links: NavItem[];
-  prefetch: boolean;
+type BaseLinkListProps = {
+  prefetch?: boolean;
   slugPrefix: string;
-  linkType: LinkType;
-}) {
-  return links.map((value) => {
-    return (
-      <Link
-        key={value.title}
-        href={slugPrefix + value.slug}
-        prefetch={prefetch}
-        className="cursor-pointer"
-      >
-        <div className="bg-gray-dark hover:border-primary my-2 flex h-10 items-center justify-between rounded-lg px-4 hover:border-2">
-          {linkTemplates[linkType](value as any)}
-        </div>
-      </Link>
-    );
-  });
+};
+
+type ArticleLinkListProps = BaseLinkListProps & {
+  links: ArticleNavItem[];
+  linkType: "learn";
+};
+
+type VisualizationLinkListProps = BaseLinkListProps & {
+  links: VisualizationNavItem[];
+  linkType: "visualize";
+};
+
+type ProblemLinkListProps = BaseLinkListProps & {
+  links: ProblemNavItem[];
+  linkType: "solve";
+};
+
+type LinkListProps =
+  | ArticleLinkListProps
+  | VisualizationLinkListProps
+  | ProblemLinkListProps;
+
+const linkTemplate = (
+  linkType: "learn" | "visualize" | "solve",
+  links: ArticleNavItem | VisualizationNavItem | ProblemNavItem,
+) => {
+  if (linkType === "learn") {
+    return linkTemplates.learn(links);
+  } else if (linkType === "visualize") {
+    return linkTemplates.visualize(links);
+  } else if (linkType === "solve" && "difficulty" in links) {
+    return linkTemplates.solve(links);
+  }
+};
+
+export function LinkList(props: LinkListProps) {
+  const { prefetch = false, slugPrefix } = props;
+
+  return props.links.map((value) => (
+    <Link
+      key={value.title}
+      href={slugPrefix + value.slug}
+      prefetch={prefetch}
+      className="cursor-pointer"
+    >
+      <div className="bg-gray-dark hover:border-primary my-2 flex h-10 items-center justify-between rounded-lg px-4 hover:border-2">
+        {linkTemplate(props.linkType, value)}
+      </div>
+    </Link>
+  ));
 }
