7d7e3b79b826637d2e1611255ba6411a39c780f7
Łukasz Radecki <69908232+Lukas249@users.noreply.github.com>
2026-05-09 17:28:20 +0200

    fix(zoom): remove double-click zoom behavior

--- app/visualize/graph-visualization/d3/zoom.ts
+++ app/visualize/graph-visualization/d3/zoom.ts
@@ -10,5 +10,7 @@ export function addZoomBehavior(
     container.attr("transform", event.transform);
     zoomRef.current.transform = event.transform;
   });
-  svg.call(zoom as any);
+  
+  svg.call(zoom as any)
+    .on("dblclick.zoom", null);
 }
